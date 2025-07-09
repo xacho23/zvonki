b35636452dfdc1bbb7ef8e02d7b1e7c185cb0feb
xacho23 <128095817+xacho23@users.noreply.github.com>
2025-07-10 01:24:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -113,6 +113,7 @@ function getUsersStatus() {
   }));
 }
 
-server.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
